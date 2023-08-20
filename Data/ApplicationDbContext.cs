f936b93ed032e75ab981e82cdbe12f008f714caa
carlosbernardocharles <carlosbernardocharles@gmail.com>
2023-08-21 01:41:16 +0200

    Entity FrameWork Migration data

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkWeb.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
