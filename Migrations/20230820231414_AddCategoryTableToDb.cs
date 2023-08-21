affec3c30bca6e95ba9f1165d5b36589584c2b96
carlosbernardocharles <carlosbernardocharles@gmail.com>
2023-08-21 14:03:52 +0200

    connect db Migration add data and retrieve from db

--- Migrations/20230820231414_AddCategoryTableToDb.cs
+++ Migrations/20230820231414_AddCategoryTableToDb.cs
@@ -16,7 +16,7 @@ namespace BulkWeb.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "int", nullable: false),
                     DisplayOrder = table.Column<string>(type: "nvarchar(max)", nullable: false)
                 },
                 constraints: table =>
--- Migrations/20230821110524_seedCategoryTable.Designer.cs
+++ Migrations/20230821110524_seedCategoryTable.Designer.cs
@@ -3,6 +3,7 @@ using BulkWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BulkWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230821110524_seedCategoryTable")]
+    partial class seedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
