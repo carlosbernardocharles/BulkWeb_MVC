b79ac66cbc302553c68f72f3aec6afcdafdc8baa
carlosbernardocharles <carlosbernardocharles@gmail.com>
2023-08-27 13:13:22 +0200

    Category Crud done

--- Controllers/Category.cs
+++ Controllers/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkWeb.Controllers
 {
@@ -7,7 +8,11 @@ namespace BulkWeb.Controllers
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(100)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Category")]
+        [Range(1,100,ErrorMessage ="Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -15,5 +15,100 @@ namespace BulkWeb.Controllers
             List<Category> categories = _db.Categories.ToList();
             return View(categories);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj">Category Object</param>
+        /// <returns> view of index page</returns>
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid) { 
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+                TempData["succsess"] = "Category Created successfully";
+            }
+            return RedirectToAction("Index", "Category");
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"> this id referes to the category on the DB</param>
+        /// <returns></returns>
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            //find category by ID
+            Category categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj">Category Object</param>
+        /// <returns> view of index page</returns>
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["succsess"] = "Category Updated successfully";
+            }
+            return RedirectToAction("Index", "Category");
+        }
+
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //find category by ID
+            Category categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj">Category Object</param>
+        /// <returns> view of index page</returns>
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["succsess"] = "Category Removed successfully";
+            return RedirectToAction("Index", "Category");
+        }
     }
 }
